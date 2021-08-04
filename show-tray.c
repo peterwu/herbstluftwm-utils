fef0261683f35da4a1d2f67de01a27d9d9c65995
Peter Wu <peterwu@hotmail.com>
2021-08-04 11:22:21 -0400

    apply clangd code format

--- show-tray.c
+++ show-tray.c
@@ -6,18 +6,15 @@ const int height = 360;
 const int posy = 30;
 const int posx = (screen_width - width) / 2;
 
-void create_window() {
+void create_window() {}
 
-}
+gint main(gint argc, gchar *argv[]) {
+  setenv("GTK_THEME", "Adwaita:dark", 1);
 
-gint
-main(gint argc, gchar *argv[]) {
-    setenv("GTK_THEME", "Adwaita:dark", 1);
+  gtk_init(&argc, &argv);
+  gdk_set_program_class("hlwm-show-tray");
+  create_window();
+  gtk_main();
 
-    gtk_init(&argc, &argv);
-    gdk_set_program_class("hlwm-show-tray");
-    create_window();
-    gtk_main();
-
-    return 0;
+  return 0;
 }
